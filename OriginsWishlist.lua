4d39ae9faea4fae8ede6f17bb0158dd945e5e073
Yann Lugrin <yann.lugrin@antistatique.net>
2021-06-09 00:16:58 +0200

    Initial Release

---
+++ OriginsWishlist.lua
@@ -0,0 +1,15 @@
+local function addOriginsWishlist(tooltip)
+	local itemName, itemLink = tooltip:GetItem()
+
+	if itemLink then
+		local itemID = itemLink:match("item:(%d+):")
+
+		if OriginsWishlistExport[itemID] ~= nil then
+			tooltip:AddLine("\nOrïgins Wishlist")
+			tooltip:AddLine(OriginsWishlistExport[itemID])
+		end
+	end
+end
+
+GameTooltip:HookScript("OnTooltipSetItem", addOriginsWishlist)
+ItemRefTooltip:HookScript("OnTooltipSetItem", addOriginsWishlist)
