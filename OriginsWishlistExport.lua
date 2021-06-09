865ce12cf6ac059389acaac2c4a4b2a0f75a8cca
Yann Lugrin <yann.lugrin@antistatique.net>
2021-06-09 15:05:52 +0200

    Update export (including T4 tokens)

--- OriginsWishlistExport.lua
+++ OriginsWishlistExport.lua
@@ -1,20 +1,31 @@
 OriginsWishlistExport = {}
 
 -- Quest items
-OriginsWishlistExport["32385"] = "|cffffffffÐeadscream|r, |cffff7c0aDjädjäx|r, |cff0070ddKïtwik|r, |cffc69b6dNarcose|r, |cfff48cbaPolka|r, |cfff48cbaSiven|r"
+OriginsWishlistExport["32385"] = "|cffffffffÐeadscream|r, |cffff7c0aDjädjäx|r, |cff0070ddKïtwik|r, |cffc69b6dNarcose|r, |cfff48cbaPolka|r, |cfff48cbaSiven|r, |cfff48cbaVelnar|r"
+OriginsWishlistExport["29754"] = "|cfff48cbaPolka|r, |cfff48cbaSiven|r, |cfff48cbaVelnar|r"
+OriginsWishlistExport["29753"] = "|cffc69b6dNarcose|r"
+OriginsWishlistExport["29763"] = "|cff0070ddKïtwik|r, |cfff48cbaPolka|r, |cfff48cbaSiven|r, |cfff48cbaVelnar|r"
+OriginsWishlistExport["29764"] = "|cffc69b6dAmarokk|r, |cffff7c0aDjädjäx|r, |cffc69b6dNarcose|r"
+OriginsWishlistExport["29758"] = "|cffc69b6dAmarokk|r, |cffff7c0aDjädjäx|r, |cffc69b6dNarcose|r"
+OriginsWishlistExport["29760"] = "|cff0070ddKïtwik|r, |cfff48cbaPolka|r, |cfff48cbaSiven|r, |cfff48cbaVelnar|r"
+OriginsWishlistExport["29761"] = "|cffc69b6dAmarokk|r, |cffff7c0aDjädjäx|r, |cffc69b6dNarcose|r, |cffffffffStarfiyaz|r"
+OriginsWishlistExport["29766"] = "|cfff48cbaVelnar|r"
+OriginsWishlistExport["29767"] = "|cffc69b6dAmarokk|r, |cffff7c0aDjädjäx|r"
 
 -- Stuff items
 OriginsWishlistExport["19395"] = "|cfff48cbaSiven|r"
-OriginsWishlistExport["28502"] = "|cffc69b6dAmarokk|r, |cffc69b6dNarcose|r"
+OriginsWishlistExport["19431"] = "|cfff48cbaVelnar|r"
+OriginsWishlistExport["28502"] = "|cffc69b6dAmarokk|r, |cffc69b6dNarcose|r, |cfff48cbaVelnar|r"
 OriginsWishlistExport["28505"] = "|cfff48cbaPolka|r, |cfff48cbaSiven|r"
 OriginsWishlistExport["28508"] = "|cffffffffStarfiyaz|r"
 OriginsWishlistExport["28511"] = "|cffffffffStarfiyaz|r"
 OriginsWishlistExport["28512"] = "|cfff48cbaPolka|r, |cfff48cbaSiven|r"
-OriginsWishlistExport["28516"] = "|cffc69b6dAmarokk|r, |cffc69b6dNarcose|r"
+OriginsWishlistExport["28516"] = "|cffc69b6dAmarokk|r, |cffc69b6dNarcose|r, |cfff48cbaVelnar|r"
+OriginsWishlistExport["28518"] = "|cfff48cbaVelnar|r"
 OriginsWishlistExport["28520"] = "|cff0070ddKïtwik|r"
 OriginsWishlistExport["28523"] = "|cff0070ddKïtwik|r"
 OriginsWishlistExport["28528"] = "|cffc69b6dAmarokk|r, |cffc69b6dNarcose|r"
-OriginsWishlistExport["28566"] = "|cffc69b6dAmarokk|r, |cffc69b6dNarcose|r"
+OriginsWishlistExport["28566"] = "|cffc69b6dAmarokk|r, |cffc69b6dNarcose|r, |cfff48cbaVelnar|r"
 OriginsWishlistExport["28569"] = "|cfff48cbaSiven|r"
 OriginsWishlistExport["28570"] = "|cffffffffÐeadscream|r"
 OriginsWishlistExport["28588"] = "|cffffffffStarfiyaz|r"
@@ -42,11 +53,12 @@ OriginsWishlistExport["28771"] = "|cff0070ddKïtwik|r, |cfff48cbaPolka|r, |cfff4
 OriginsWishlistExport["28780"] = "|cffffffffÐeadscream|r"
 OriginsWishlistExport["28789"] = "|cffffffffÐeadscream|r"
 OriginsWishlistExport["28790"] = "|cff0070ddKïtwik|r, |cfff48cbaPolka|r, |cfff48cbaSiven|r"
-OriginsWishlistExport["28792"] = "|cffff7c0aDjädjäx|r, |cffc69b6dNarcose|r"
+OriginsWishlistExport["28792"] = "|cffff7c0aDjädjäx|r, |cffc69b6dNarcose|r, |cfff48cbaVelnar|r"
 OriginsWishlistExport["28793"] = "|cffffffffÐeadscream|r"
 OriginsWishlistExport["28799"] = "|cffffffffÐeadscream|r"
+OriginsWishlistExport["28802"] = "|cfff48cbaVelnar|r"
 OriginsWishlistExport["28822"] = "|cfff48cbaPolka|r, |cfff48cbaSiven|r"
-OriginsWishlistExport["28825"] = "|cffc69b6dAmarokk|r, |cffc69b6dNarcose|r"
+OriginsWishlistExport["28825"] = "|cffc69b6dAmarokk|r, |cffc69b6dNarcose|r, |cfff48cbaVelnar|r"
 OriginsWishlistExport["28830"] = "|cffff7c0aDjädjäx|r"
 OriginsWishlistExport["29011"] = "|cffc69b6dAmarokk|r, |cffc69b6dNarcose|r"
 OriginsWishlistExport["29012"] = "|cffc69b6dNarcose|r"
@@ -59,13 +71,65 @@ OriginsWishlistExport["29049"] = "|cffffffffStarfiyaz|r"
 OriginsWishlistExport["29061"] = "|cfff48cbaPolka|r, |cfff48cbaSiven|r"
 OriginsWishlistExport["29062"] = "|cfff48cbaPolka|r, |cfff48cbaSiven|r"
 OriginsWishlistExport["29064"] = "|cfff48cbaPolka|r, |cfff48cbaSiven|r"
+OriginsWishlistExport["29066"] = "|cfff48cbaVelnar|r"
+OriginsWishlistExport["29068"] = "|cfff48cbaVelnar|r"
+OriginsWishlistExport["29069"] = "|cfff48cbaVelnar|r"
+OriginsWishlistExport["29070"] = "|cfff48cbaVelnar|r"
 OriginsWishlistExport["29096"] = "|cffff7c0aDjädjäx|r"
 OriginsWishlistExport["29097"] = "|cffff7c0aDjädjäx|r"
 OriginsWishlistExport["29098"] = "|cffff7c0aDjädjäx|r"
 OriginsWishlistExport["29099"] = "|cffff7c0aDjädjäx|r"
 OriginsWishlistExport["29100"] = "|cffff7c0aDjädjäx|r"
-OriginsWishlistExport["29279"] = "|cffc69b6dAmarokk|r, |cffff7c0aDjädjäx|r, |cffc69b6dNarcose|r"
+OriginsWishlistExport["29279"] = "|cffc69b6dAmarokk|r, |cffff7c0aDjädjäx|r, |cffc69b6dNarcose|r, |cfff48cbaVelnar|r"
 OriginsWishlistExport["29290"] = "|cffffffffStarfiyaz|r"
 OriginsWishlistExport["29458"] = "|cff0070ddKïtwik|r, |cfff48cbaPolka|r, |cfff48cbaSiven|r"
 OriginsWishlistExport["30666"] = "|cffffffffÐeadscream|r"
 OriginsWishlistExport["30674"] = "|cffff7c0aDjädjäx|r"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
